a5ffd51e6b289e061f317a275e8175c98abc90c4
LampSeeker <rampseeker@gmail.com>
2026-05-26 13:47:12 +0900

    Use manual multi-database mounts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,8 +3,23 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
   base_url: "https://lampseeker.github.io",
   mount: {
-    manual: false,
-    page_url: "https://www.notion.so/rampseeker/Notion-Hugo-ec55b029aea6837e811d017848583280",
+    manual: true,
+    databases: [
+      {
+        database_id: "f695b029-aea6-82ea-a34f-016e7e6cfd45",
+        target_folder: "posts",
+      },
+      {
+        database_id: "36c5b029-aea6-8015-b401-e4f78b3ec744",
+        target_folder: "projects",
+      },
+    ],
+    pages: [
+      {
+        page_id: "2f35b029-aea6-83dd-9283-016b5bce45d7",
+        target_folder: ".",
+      },
+    ],
   },
 }
 
